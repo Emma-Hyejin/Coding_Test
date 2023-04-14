d9abbc5a24f151aa84f0daaee29a2a0308f76a54
Emma-Hyejin <cjd191058@naver.com>
2023-04-14 23:33:45 +0900

    LV01_정수 내림차순으로 배치하기

---
+++ programmers_LV_01/place_integers_indesc.js
@@ -0,0 +1,13 @@
+//정수 내림차순으로 배치하기
+//입력 : 118372
+//출력 : 873211
+//[1] 내 풀이 
+function solution(n) {
+    var answer = 0; 
+    n = String(n).split(''); // n을 str타입으로 하나씩 분해해서 배열로 만듬.
+    n.sort(function(a,b){return a-b});  // sort로 작은 순서대로 정렬
+    n = n.reverse().join(''); // reverse로 내림차순으로 정렬 후 join으로 문자열로 합치기
+
+    answer = Number(n); // 숫자로 출력해야 하므로 Number()로 변환.
+    return answer;
+}
