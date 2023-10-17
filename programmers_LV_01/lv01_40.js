ee3214fa1a532527672e4d0838ff7fa3791c4145
Emma-Hyejin <cjd191058@naver.com>
2023-10-17 20:25:00 +0900

    LV01: 가장 가까운 글자

--- programmers_LV_01/lv01_40.js
+++ programmers_LV_01/lv01_40.js
@@ -1,30 +1,23 @@
-//문자열 내 마음대로 정렬하기 
-//input = ['sun', 'bed', 'car'], n = 1; return = ["car", "bed", "sun"]
-// 각 문자열 요소의 [n] 위치를 기준으로 정렬 
-
-function solution(strings, n) {
+//가장 가까운 같은 글자 
+function solution(s) {
+    //앞에서부터 배열에 넣어서 shift(); 위치 + 1 
+    //findInex 찾은 요소의 index 반환 , 첫 번째 위치 반환 함수만 인수로 받음
+    //indexOf
+    let temp = [];
     let answer = [];
-
-    answer = strings.sort((acc, cur) => {
-        if(acc[n] !== cur[n]){
-            //오름 차순
-            return acc[n] > cur[n] ? 1 : -1
-        };
-
-        return acc > cur ? 1 : -1
-    });
-
+    
+    for( let i = 0 ; i < s.length; i ++){
+        if(temp.indexOf(s[i]) === -1){
+            temp.unshift(s[i]);
+            answer.push(-1);
+            
+        }else{
+            const index = temp.indexOf(s[i]) + 1 ;
+            console.log(index)
+            answer.push(index);
+            temp.unshift(s[i])
+        }
+    }
+    //console.log(answer);
     return answer;
 }
-
-//미친놈 코드 하나 발견함 
-
-function solution(strings, n){
-    return strings.sort((acc, cur) => acc[n] === cur[n] ? acc.localeCompare(cur) : acc[n].localeCompare(cur[n]));
-
-};
-
-//**localeCompare : 
-//문자열의 사전상 순서에서 해당 문잦가 뒤에 있는니 앞에 있는지  혹은 같은 지 판단해서 1, 0, -1을 반환 
-//오름차순 : arr.sort((a, b) => a.localeCompare(b));
-//내림차순 : arr.sort((a, b) => b.localeCompare(a));
