3b0074a52507bc71404f3ce9377b791222e64cb9
Emma-Hyejin <cjd191058@naver.com>
2023-11-15 22:27:04 +0900

    Lv01 - 소수 만들기

---
+++ programmers_LV_01/lv01_54.js
@@ -0,0 +1,54 @@
+//소수 만들기 - Summer/Winter Coding (~2018)
+//재귀 조합 알고리즘 
+function combination (arr, num) {
+    const results = [];
+    
+    //nC1 이면 1
+    if( num === 1) return arr.map(v => [v]);
+    
+    arr.forEach((fixed, index, origin )=> {
+        //순서에 상관없이 중복이 되면 안됩니다. 현재 값 이후의 배열들만 추출
+        const rest = origin.slice(index + 1);
+        
+        //나머지 배열을 기준으로 다시 조합 실시
+        //기준값 fixed 때문에 선택 개수 -1
+        const combinations = combination(rest, num-1);
+        
+        //기준값 fixed 에 돌아온 조합을 붙이기
+        const attached = combinations.map(v => [fixed, ...v]);
+        
+        //붙인 값을 결과 에 넣어주기 
+        results.push(...attached);
+    });
+    
+    return results;
+}
+
+function isPrime (num) {
+    if(num < 2) return false;
+    for( let i = 2; i < num; i++){
+        if( num % i === 0){
+            return false;
+        }
+    };
+    
+    return true;
+}
+
+function solution(nums) {
+    var answer = 0;
+    
+    const combinationArr = combination(nums, 3);
+    
+    combinationArr.forEach((el) => {
+        const sum = el.reduce((acc, cur) => acc + cur, 0);
+        if( isPrime(sum) ){
+            answer ++;
+        }
+
+    })
+
+
+    
+    return answer;
+}
